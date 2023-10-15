9994c1afb97d9a5dacb92aeafa09109cee7fe3c3
Lohit Akkineni <lohit.akkineni@slu.edu>
2023-10-15 03:42:45 -0500

    Update index.tsx

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,8 +1,25 @@
-import * as React from 'react';
-import styles from '@/components/Card/Card.module.scss';
+import React from 'react';
 
-export const Card = (props: React.PropsWithChildren<{ color: string }>) => (
-  <div className={styles.Card} style={{ backgroundColor: props.color }}>
-    {props.children}
-  </div>
-);
+// Define the Card component with the onClick prop
+interface CardProps {
+  color: string;
+  onClick?: () => void; // Add onClick prop here
+  children: React.ReactNode;
+}
+
+const Card: React.FC<CardProps> = ({ color, onClick, children }) => {
+  // Use onClick handler if provided
+  const handleClick = () => {
+    if (onClick) {
+      onClick();
+    }
+  };
+
+  return (
+    <div style={{ backgroundColor: color, padding: '1rem', cursor: 'pointer' }} onClick={handleClick}>
+      {children}
+    </div>
+  );
+};
+
+export default Card;
