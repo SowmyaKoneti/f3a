85b449d8423c5d02ca4f179e25ec8f8da698738f
Lohit Akkineni <lohit.akkineni@slu.edu>
2023-10-15 03:35:33 -0500

    Update CustomCard.tsx

--- src/pages/About/CustomCard.tsx
+++ src/pages/About/CustomCard.tsx
@@ -1,1 +1,18 @@
+// CustomCard.js
+import React, { type ReactNode } from 'react';
 
+interface CustomCardProps {
+  children: ReactNode;
+  color: string;
+  onClick: () => void;
+}
+
+const CustomCard = ({ children, color, onClick }: CustomCardProps) => {
+  return (
+    <div className="your-card-class" style={{ backgroundColor: color }} onClick={onClick}>
+      {children}
+    </div>
+  );
+};
+
+export default CustomCard;
