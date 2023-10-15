d085ce371e574380307b3c16909f8ed8fac527fe
Lohit Akkineni <lohit.akkineni@slu.edu>
2023-10-15 03:34:45 -0500

    Update index.tsx

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,16 +1,35 @@
-import { Card } from '@/components/Card';
+import React from 'react';
+import Card from '@/components/Card'; // Import the Card component
+
 import styles from '@/pages/About/About.module.scss';
 
-export const About = () => (
-  <div className={styles.About}>
-    <h1>About Page</h1>
-    <p>Cards List</p>
-    <div className={styles['cards-list']}>
-      <Card color="black">Card #1</Card>
-      <Card color="darkgray">Card #2</Card>
-      <Card color="brown">Card #3</Card>
-      <Card color="red">Card #4</Card>
-      <Card color="blue">Card #5</Card>
+export const About = () => {
+  const handleTeamClick = (teamName: string) => {
+    // Handle the click event for the team card here
+    console.log(`Clicked on ${teamName}`);
+  };
+
+  return (
+    <div className={styles.About}>
+      <h1>Player Availability</h1>
+      <p>Click on the respective team card to know the player availability of each team</p>
+      <div className={styles['cards-list']}>
+        {/* Use the Card component and pass the onClick prop */}
+        <Card color="black" onClick={() => handleTeamClick('Team 1')}>
+          Team 1
+        </Card>
+        <Card color="purple" onClick={() => handleTeamClick('Team 2')}>
+          Team 2
+        </Card>
+        <Card color="blue" onClick={() => handleTeamClick('Team 3')}>
+          Team 3
+        </Card>
+        <Card color="orange" onClick={() => handleTeamClick('Team 4')}>
+          Team 4
+        </Card>
+      </div>
     </div>
-  </div>
-);
+  );
+};
+
+export default About;
